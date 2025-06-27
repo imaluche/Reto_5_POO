a41bd399bc21d7f59de37d42fede7bd95361431b
imaluche <159048470+imaluche@users.noreply.github.com>
2025-06-27 16:18:12 -0500

    Add files via upload

---
+++ reto-5_1/main.py
@@ -0,0 +1,16 @@
+from shape.shape_1 import *
+if __name__ == "__main__":
+    p0= Point(0, 0)
+    p1= Point(0, 4)
+    p2= Point(-3, 0)
+    l0= Line(p0, p1)
+    l1= Line(p1, p2)
+    l3 = Line(p2,p0)
+    l : list = [l0, l1, l3]
+    escal= Trirectangle(l)
+    print(f"el perimetro es: {escal.compute_perimeter()}")
+    print(f"el area es: {escal.compute_area()}")
+    print(f"los angulos internos son: {escal._inner_angles}")
+    print(f"vertices:")
+    for i in escal._vertices:
+        print(f"({i.x},{i.y})")
